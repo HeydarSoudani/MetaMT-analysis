ae23d951e718a0e07422105db923f24ac892fc93
Heydar Soudani <heydar.soudani@gmail.com>
2021-10-17 02:28:04 +0330

    load model change

--- analysis.py
+++ analysis.py
@@ -73,7 +73,7 @@ if args.first_model != "":
 if args.second_model != "":
   second_model = torch.load(args.second_model)
 
-# print(first_model)
+print(first_model)
 # for name, param in first_model.named_parameters():
 #   print('name: {}, param: {}'.format(name, param.shape))
 # print(second_model.clf_model.roberta.encoder.layer[0].attention.output.dense.weight.shape)
