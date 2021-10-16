8f9b1b76ab51deeb3657330f51ca19372322415e
Heydar Soudani <heydar.soudani@gmail.com>
2021-10-16 17:52:35 +0330

    print hidden

--- cca_core.py
+++ cca_core.py
@@ -151,7 +151,7 @@ def compute_ccas(sigma_xx, sigma_xy, sigma_yx, sigma_yy, epsilon,
   if verbose:
     print("computed everything!")
 
-  return [u, np.abs(s), v], invsqrt_xx, invsqrt_yy, x_idxs, y_idxs
+  return u, np.abs(s), v, invsqrt_xx, invsqrt_yy, x_idxs, y_idxs
 
 
 def sum_threshold(array, threshold):
@@ -261,7 +261,7 @@ def get_cca_similarity(acts1, acts2, epsilon=0., threshold=0.98,
   sigmaxy /= np.sqrt(xmax * ymax)
   sigmayx /= np.sqrt(xmax * ymax)
 
-  [u, s, v], invsqrt_xx, invsqrt_yy, x_idxs, y_idxs = compute_ccas(sigmaxx, sigmaxy, sigmayx, sigmayy, epsilon=epsilon,verbose=verbose)
+  u, s, v, invsqrt_xx, invsqrt_yy, x_idxs, y_idxs = compute_ccas(sigmaxx, sigmaxy, sigmayx, sigmayy, epsilon=epsilon,verbose=verbose)
 
   # if x_idxs or y_idxs is all false, return_dict has zero entries
   if (not np.any(x_idxs)) or (not np.any(y_idxs)):
